ac098c6990ec52fdd967b907cfccfdcc9bac95d6
lumafepe <lumafepe@gmail.com>
2025-05-01 21:08:34 +0100

    feat: reddit support

--- app/core/preprocessing.py
+++ app/core/preprocessing.py
@@ -1,10 +1,18 @@
 import os
+from typing import List
 import instaloader
 import os
 from pathlib import Path
 import shutil
 from google import genai
 import time
+import tweepy
+import praw
+import requests
+
+if __name__ == "__main__":
+    from dotenv import load_dotenv
+    load_dotenv()
 
 allowed_gemini_extensions = [".png", ".jpg", ".txt", ".mp4"]
 
@@ -27,10 +35,13 @@ def gemini_wait_until_active(client, name, timeout=60):
         time.sleep(1)
     raise TimeoutError(f"File {name} did not become ACTIVE in time.")
 
+
+
+
+
 def summarize_insta_post(shortcode: str) -> str:
     base_dir = f"{os.getenv("TEMPORARY_DIRECTORY")}/insta"
     directory = f"{base_dir}/{shortcode}"
-    client = genai.Client(api_key=os.getenv("API_KEY"))
 
     files = [ 
         os.path.join(directory, f) for f in os.listdir(directory) 
@@ -41,6 +52,8 @@ def summarize_insta_post(shortcode: str) -> str:
         f for f in files
         if Path(f).suffix in allowed_gemini_extensions
     ]
+    
+    client = genai.Client(api_key=os.getenv("API_KEY"))
     
     uploaded_files = [client.files.upload(file=f) for f in filtered_files]
     content = uploaded_files + ["Summarize this instagram post"]
@@ -68,3 +81,84 @@ def insta_post(shortcode: str) -> str:
         summary = summarize_insta_post(shortcode)
     finally:
         delete_insta_post(shortcode)
+
+
+
+reddit = praw.Reddit(
+    client_id=os.getenv("REDDIT_CLIENT_ID"),
+    client_secret=os.getenv("REDDIT_CLIENT_SECRET"),
+    user_agent="my_reddit_app"
+)
+
+
+def get_reddit_post_data(post_id: str) -> dict:
+    submission = reddit.submission(id=post_id)
+    result = {
+        "title": submission.title,
+        "text": submission.selftext or None,
+    }
+
+    if submission.is_reddit_media_domain and submission.url:
+        result["media"] = [submission.url]
+    elif submission.media and "reddit_video" in submission.media:
+        result["media"] = [submission.media["reddit_video"]["fallback_url"]]
+    elif hasattr(submission, "preview") and "images" in submission.preview:
+        result["media"] = [img["source"]["url"] for img in submission.preview["images"]]
+    return result
+
+
+def download_media_reddit(shortcode : str,media: List[str]):
+    base_dir = f"{os.getenv("TEMPORARY_DIRECTORY")}/reddit"
+    directory = f"{base_dir}/{shortcode}"
+    os.makedirs(directory, exist_ok=True)
+    for i, url in enumerate(media):
+        response = requests.get(url)
+        file_type = url.split(".")[-1]
+        if response.status_code == 200:
+            with open(f"{directory}/{i}.{file_type}", "wb") as f:
+                f.write(response.content)
+        else:
+            print(f"Failed to download {url}")
+            
+
+def summarize_reddit_post(shortcode: str,data : dict) -> str:
+    base_dir = f"{os.getenv("TEMPORARY_DIRECTORY")}/reddit"
+    directory = f"{base_dir}/{shortcode}"
+    
+    files = [ 
+        os.path.join(directory, f) for f in os.listdir(directory) 
+        if os.path.isfile(os.path.join(directory, f))
+    ]
+
+    filtered_files = [
+        f for f in files
+        if Path(f).suffix in allowed_gemini_extensions
+    ]
+    client = genai.Client(api_key=os.getenv("API_KEY"))
+    
+    uploaded_files = [client.files.upload(file=f) for f in filtered_files]
+    content = uploaded_files + [f"Summarize this reddit post with the following data: title: {data['title']}, text: {data['text']}"]
+
+    # Wait for files to be uploaded
+    for f in uploaded_files:
+        gemini_wait_until_active(client, f.name)
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash", contents=content
+    )
+
+    return response.text
+
+def delete_reddit_post(shortcode: str):
+    base_dir = f"{os.getenv("TEMPORARY_DIRECTORY")}/reddit"
+    directory = f"{base_dir}/{shortcode}"
+    shutil.rmtree(directory)
+
+def reddit_post(shortcode: str) -> dict:
+    try:
+        data = get_reddit_post_data(shortcode)
+        if "media" in data:
+            download_media_reddit(shortcode, data["media"])
+            return summarize_reddit_post(shortcode,data)
+    finally:
+        delete_reddit_post(shortcode)
