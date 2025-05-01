6a014b05636ecd075137b67a3c45b4a7ba93f402
lumafepe <lumafepe@gmail.com>
2025-05-01 18:21:52 +0100

    feat: fact check

---
+++ core/core.py
@@ -0,0 +1,16 @@
+from .google_fact_check import get_reviews_text
+from .reviews_processing import process_reviews
+
+def fact_check(query: str) -> dict:
+    """
+    Perform a fact check on the given query using Google Fact Check API and process the reviews.
+    
+    Args:
+        query (str): The query to be fact-checked.
+        
+    Returns:
+        dict: A dictionary containing the results of the fact check.
+    """
+    reviews = get_reviews_text(query)
+    result = process_reviews(query, reviews)
+    return result
